bc8e6da93b87ea37b35a8a4def6777ca14f39606
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-24 09:14:30 -0500

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(cors());
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('drawing', (data) => {
+    socket.broadcast.emit('drawing', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
